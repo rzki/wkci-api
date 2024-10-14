1bc3070865d8a0754e8beefefefbf615141b7932
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-14 09:35:55 +0700

    route change

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/1.0/generate-token', [YukkApiController::class, 'generateAccessToken']);
-Route::get('/1.0/generate-qr', [YukkApiController::class, 'generateQR']);
-Route::get('/1.0/access-token/b2b', [YukkApiController::class, 'generateAccessTokenForYUKK']);
-Route::post('/1.0/qr-notification', [YukkApiController::class, 'paymentNotification']);
+Route::get('/v1.0/generate-token', [YukkApiController::class, 'generateAccessToken']);
+Route::get('/v1.0/generate-qr', [YukkApiController::class, 'generateQR']);
+Route::get('/v1.0/access-token/b2b', [YukkApiController::class, 'generateAccessTokenForYUKK']);
+Route::post('/v1.0/qr/qr-mpm-notify', [YukkApiController::class, 'paymentNotification']);
