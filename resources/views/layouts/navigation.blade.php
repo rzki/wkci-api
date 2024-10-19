c0d5b38bffe07bfa79f945948640cf44dab6f561
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-19 15:54:17 +0700

    change dashboard route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,25 +1,27 @@
 <ul class="nav flex-column pt-3 pt-md-0">
     <li class="nav-item">
-        <a href="{{ route('home') }}" class="nav-link d-flex align-items-center">
-            <span class="sidebar-icon me-3">
-                <img src="{{ asset('images/brand/light.svg') }}" height="20" width="20" alt="Volt Logo">
-            </span>
+        <a href="{{ route('dashboard') }}" class="nav-link d-flex align-items-center">
             <span class="mt-1 ms-1 sidebar-text">
-                Volt Laravel
+                JADE 2024
             </span>
         </a>
     </li>
 
-    <li class="nav-item {{ request()->routeIs('home') ? 'active' : '' }}">
-        <a href="{{ route('home') }}" class="nav-link">
-            <span class="sidebar-icon">
-                <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                    <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                </svg>
+    <li class="nav-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+        <a href="{{ route('dashboard') }}" class="nav-link"wire:navigate>
+            <span class="sidebar-icon me-3">
+                <i class="fas fa-chart-area"></i>
             </span>
             <span class="sidebar-text">{{ __('Dashboard') }}</span>
+        </a>
+    </li>
+
+    <li class="nav-item {{ request()->routeIs('products.index') ? 'active' : '' }}" >
+        <a href="{{ route('products.index') }}" class="nav-link"wire:navigate>
+            <span class="sidebar-icon me-3">
+                <i class="fas fa-boxes"></i>
+            </span>
+            <span class="sidebar-text">{{ __('Products') }}</span>
         </a>
     </li>
 
@@ -77,3 +79,4 @@
         </div>
     </li>
 </ul>
+
