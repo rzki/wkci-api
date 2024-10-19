240c3c9cc096a70dfa2eefbce6c6907d97e7db7a
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-19 15:43:01 +0700

    change dashboard route

---
+++ app/Livewire/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+
+class Dashboard extends Component
+{
+    #[Layout('components.layouts.app')]
+    #[Title('Dashboard')]
+    public function render()
+    {
+        return view('livewire.dashboard');
+    }
+}
