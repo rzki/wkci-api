f500355f97700b967662c5151303952ab7a1b942
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-22 16:41:30 +0700

    updated route

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/dashboard', Dashboard::class)->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::view('about', 'about')->name('about');
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
     Route::get('products', ProductIndex::class)->name('products.index');
     Route::get('products/create', ProductCreate::class)->name('products.create');
     Route::get('products/edit/{productId}', ProductEdit::class)->name('products.edit');
@@ -29,9 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::get('forms/{formId}', FormEdit::class)->name('forms.edit');
     Route::get('forms/import', FormImport::class)->name('forms.import');
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 });
+
 Route::get('/register/hands-on', HandsOnForm::class)->name('hands-on_form');
 Route::get('/register/seminar', SeminarForm::class)->name('seminar_form');
