403e5305828e471dff9a550813de094ee749d179
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-28 15:54:13 +0700

    remove register link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
             <div class="col-12 d-flex align-items-center justify-content-center">
                 <div class="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
                     <div class="text-center text-md-center mb-4 mt-md-0">
-                        <h1 class="mb-3 h3">{{ __('Welcome back') }}</h1>
+                        <h1 class="mb-3 h3">{{ __('Jakarta Dental Exhibition (JADE) 2024') }}</h1>
                     </div>
 
                     <form class="mt-4" action="{{ route('login') }}" method="POST">
@@ -53,17 +53,12 @@
                                 @enderror
                             </div>
                             <!-- End of Form -->
-                            <div class="d-flex justify-content-between align-items-top mb-4">
+                            <div class="d-flex justify-content-center align-items-top mb-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember">
                                     <label class="form-check-label mb-0" for="remember">
                                         {{ __('Remember me') }}
                                     </label>
-                                </div>
-                                <div>
-                                    <a href="{{ route('password.request') }}" class="small text-right">
-                                        {{ __('Lost password?') }}
-                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -72,12 +67,6 @@
                             <button type="submit" class="btn btn-gray-800">{{ __('Sign in') }}</button>
                         </div>
                     </form>
-                    <div class="d-flex justify-content-center align-items-center mt-4">
-                        <span class="fw-normal">
-                            {{ __('Not registered?') }}
-                            <a href="{{ route('register') }}" class="fw-bold">{{ __('Create account') }}</a>
-                        </span>
-                    </div>
                 </div>
             </div>
         </div>
