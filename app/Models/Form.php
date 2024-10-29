709314d94b721d73f2d9ecc4e979604fef582b51
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-29 14:56:43 +0700

    add search function

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -9,4 +9,11 @@ class Form extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+    public function scopeSearch($query, $value)
+    {
+        $query->where('full_name', 'like', "%{$value}%")
+        ->orWhere('name_str', 'like', "%{$value}%")
+        ->orWhere('nik', 'like', "%{$value}%")
+        ->orWhere('npa', 'like', "%{$value}%");
+    }
 }
