f6a7e90060190587d482dfca8dddc60ec3c8d9ed
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-31 16:14:21 +0700

    remove unique key to email

--- database/migrations/2024_10_23_031541_create_form_participants_table.php
+++ database/migrations/2024_10_23_031541_create_form_participants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->uuid('formId');
             $table->string('full_name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('phone');
             $table->string('origin');
             $table->tinyText('barcode')->nullable();
