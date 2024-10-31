45f45b2a682bd25ad62ddee9593b579318e596ca
Rizky Dhani <rizkydhani15@gmail.com>
2024-10-31 10:59:16 +0700

    added laravel-permission

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,9 @@
 
 use App\Providers\AppServiceProvider;
 use Spatie\Menu\Laravel\MenuServiceProvider;
+use Spatie\Permission\PermissionServiceProvider;
 
 return [
     AppServiceProvider::class,
-    MenuServiceProvider::class
+    PermissionServiceProvider::class
 ];
