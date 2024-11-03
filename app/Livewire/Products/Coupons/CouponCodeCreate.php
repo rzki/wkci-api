ab85f8f877d9661d2a97a692224b73518e860d6b
Rizky Dhani <rizkydhani15@gmail.com>
2024-11-03 17:55:43 +0700

    added Coupon Resource

---
+++ app/Livewire/Products/Coupons/CouponCodeCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Products\Coupons;
+
+use Livewire\Component;
+
+class CouponCodeCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.products.coupons.coupon-code-create');
+    }
+}
