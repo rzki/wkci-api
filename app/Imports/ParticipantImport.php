e86e7cfccee9f503db3f1f4b6b9904517b88ab87
Rizky Dhani <rizkydhani15@gmail.com>
2024-11-03 12:08:50 +0700

    added columns in ParticipantImport

--- app/Imports/ParticipantImport.php
+++ app/Imports/ParticipantImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\FormParticipant;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -11,6 +12,7 @@ use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 use Milon\Barcode\DNS2D;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 HeadingRowFormatter::default('none');
 
@@ -34,13 +36,16 @@ class ParticipantsImport implements ToModel, WithHeadingRow, SkipsEmptyRows
         $path = 'img/forms/participant/' . $uuid . '.png';
         Storage::disk('public')->put($path, $qr);
 
+        $timestampDate = Carbon::instance(Date::excelToDateTimeObject($row['Timestamp']));
+        $submitDate = $timestampDate->toDateTimeString();
         FormParticipant::create([
             'formId' => $uuid,
             'full_name' => $row['Nama'],
             'email' => $row['Email'],
             'phone' => $row['Telepon'],
             'origin' => $row['Asal'],
-            'barcode' => $path
+            'barcode' => $path,
+            'submit_date' => $submitDate,
         ]);
     }
 }
