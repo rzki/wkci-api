0d6640b2ac8d6734573c27eb44f983d5f2553dd0
Rizky Dhani <rizkydhani15@gmail.com>
2024-11-06 15:46:39 +0700

    add select2 css js

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,7 +13,9 @@
         <link rel="stylesheet" href="{{ asset('assets/sweetalert2/sweetalert2.min.css') }}">
         <!-- Styles -->
         @vite('resources/sass/app.scss')
-
+        <!-- Select2 + Bootstrap Theme -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
         <!-- Scripts -->
         @vite('resources/js/app.js')
     </head>
@@ -27,6 +29,8 @@
             </main>
 
             @yield('scripts')
+            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
             <script src="{{ asset('assets/sweetalert2/sweetalert2.all.min.js') }}"></script>
     </body>
 </html>
