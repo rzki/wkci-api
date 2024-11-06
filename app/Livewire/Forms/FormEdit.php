f2b3b0fa28cae7011cf844804b7a24d1d3bfe0ac
Rizky Dhani <rizkydhani15@gmail.com>
2024-11-06 10:34:50 +0700

    added Form Edit

--- app/Livewire/Forms/FormEdit.php
+++ app/Livewire/Forms/FormEdit.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Forms;
 
+use App\Models\Form;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Title;
@@ -10,15 +11,50 @@ use Livewire\Component;
 class FormEdit extends Component
 {
     use AuthorizesRequests;
-    public function mount()
+    public $form, $formId, $name_str, $full_name, $email, $nik, $npa, $cabang_pdgi, $phone_number, $attended;
+    public function mount($formId)
     {
         if(!Auth::user()->hasRole(['Super Admin','Admin','Finance'])) {
             abort(403, 'Unauthorized');
         }
+        $this->form = Form::where('formId', $formId)->first();
+        $this->name_str = $this->form->name_str;
+        $this->full_name = $this->form->full_name;
+        $this->email = $this->form->email;
+        $this->nik = $this->form->nik;
+        $this->npa = $this->form->npa;
+        $this->cabang_pdgi = $this->form->cabang_pdgi;
+        $this->phone_number = $this->form->phone_number;
+        $this->attended = $this->form->attended;
+    }
+    public function update()
+    {
+        Form::where('formId', $this->formId)->update([
+            'name_str' => $this->name_str,
+            'full_name' => $this->full_name,
+            'email' => $this->email,
+            'nik' => $this->nik,
+            'npa' => $this->npa,
+            'cabang_pdgi' => $this->cabang_pdgi,
+            'phone_number' => $this->phone_number,
+            'attended' => $this->attended,
+        ]);
+        session()->flash('alert', [
+            'type' => 'success',
+            'title' => 'Form successfully updated!',
+            'toast'=> true,
+            'position'=> 'top-end',
+            'timer'=> 3000,
+            'progbar' => true,
+            'showConfirmButton'=> false
+        ]);
+        return $this->redirectRoute('forms.index', navigate:true);
     }
     #[Title('Edit Forms')]
     public function render()
     {
-        return view('livewire.forms.form-edit');
+        return view('livewire.forms.form-edit', [
+            'forms' => $this->form,
+        ]);
     }
 }
--- resources/views/livewire/forms/form-edit.blade.php
+++ resources/views/livewire/forms/form-edit.blade.php
@@ -1,3 +1,81 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="main py-4">
+        <div class="row">
+            <div class="col-12 col-xl-12">
+                <div class="col-12 px-0">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <h2 class="mb-1 fs-5 fw-bold mb-5">{{ __('Edit Form') }}</h2>
+                            <div class="row mb-5">
+                                <div class="col">
+                                    <a href="{{ route('forms.index') }}" class="btn btn-primary text-white"
+                                       wire:navigate><i class="fas fa-arrow-left"></i> {{ __('Back') }}</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <form wire:submit='update'>
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="name_str" class="form-label">{{ __('Nama (Sesuai STR)') }}</label>
+                                                    <input type="text" name="name_str" id="name_str" class="form-control"
+                                                           wire:model='name_str'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="full_name" class="form-label">{{ __('Nama Lengkap') }}</label>
+                                                    <input type="text" name="full_name" id="full_name" class="form-control" wire:model='full_name'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="email" class="form-label">{{ __('Email') }}</label>
+                                                    <input type="email" name="email" id="email" class="form-control" wire:model='email'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="nik" class="form-label">{{ __('NIK') }}</label>
+                                                    <input type="text" name="nik" id="nik" class="form-control" wire:model='nik'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="npa" class="form-label">{{ __('NPA') }}</label>
+                                                    <input type="text" name="npa" id="npa" class="form-control" wire:model='npa'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="cabang_pdgi" class="form-label">{{ __('PDGI Cabang') }}</label>
+                                                    <input type="text" name="cabang_pdgi" id="cabang_pdgi" class="form-control" wire:model='cabang_pdgi'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="phone_number" class="form-label">{{ __('Nomor Telepon') }}</label>
+                                                    <input type="text" name="phone_number" id="phone_number" class="form-control" wire:model='phone_number'>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="form-group mb-3">
+                                                    <label for="attended" class="form-label">{{ __('Hands On') }}</label>
+                                                    <input type="text" name="attended" id="attended" class="form-control" wire:model='attended'>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="d-grid">
+                                            <button type="submit" class="btn btn-success text-white">{{ __('Submit') }}</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
